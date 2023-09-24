3a709b2260445231bc3472e3ee758950f84d443b
GageVanK <87737875+GageVanK@users.noreply.github.com>
2023-09-24 09:51:05 +0000

    yethr

--- src/components/Welcome.jsx
+++ src/components/Welcome.jsx
@@ -9,11 +9,25 @@ import {
   Group,
   Button,
   Divider,
+  Container,
+  SimpleGrid, rem,
+  Space
 } from "@mantine/core";
 import { IconCheck } from "@tabler/icons-react";
 import { ERROR_TYPES, identity } from "deso-protocol";
 import { GiWaveCrest } from "react-icons/gi";
+import { VscLink } from "react-icons/vsc";
+import { BiWorld } from "react-icons/bi";
+import { MdOutlineAttachMoney } from "react-icons/md";
+import { BiWorld } from "react-icons/bi";
+import { GrVirtualStorage } from "react-icons/gr";
+import { TbMoneybag } from "react-icons/tb";
+import { GrMultiple } from "react-icons/gr";
+import { PiUsersThreeDuotone } from "react-icons/pi";
 
+
+
+import { GiBigWave } from 'react-icons/gi';
 const useStyles = createStyles((theme) => ({
   inner: {
     display: "flex",
@@ -30,118 +44,125 @@ const useStyles = createStyles((theme) => ({
       marginRight: 0,
     },
   },
+
+  wrapper: {
+    paddingTop: `calc(var(${theme.spacing.xl} * 4)`,
+    paddingBottom: `calc(var(${theme.spacing.xl} * 4)`,
+  },
+
+  title: {
+    
+    fontWeight: 900,
+   
+  
+  
+  },
+
 }));
+
+
+export const MOCKDATA = [
+  {
+    icon: GiBigWave,
+    title: 'Why Waves?',
+    description:
+      'Waves is not another shitcoin project. It leverages the true power of Crypto and Blockchain technology to provide real utility to Streamers!',
+  },
+  {
+    icon: GrVirtualStorage,
+    title: 'On-Chain Storage',
+    description:
+      'Waves is built on the DeSo Blockchain, offering an alternative to private, centralized databases. It enables transparent data allowing users as much access as the builders. Traditional social platforms often use private data to sell to advertisers, but Waves provides a storage alternative to mitigate this practice.',
+  },
+  {
+    icon: BiWorld,
+    title: 'Open Source',
+    description:
+      'Waves is open source and allows for Algorithm Audits, eliminating guesswork around the magic algorithm.',
+  },
+  {
+    icon: MdOutlineAttachMoney,
+    title: 'Monetization',
+    description:
+      'Waves is powered by Deso Wallets, enabling instant Fan-to-Creator Subscription payments. No more complex monetization processes, and currently, Waves pays out 100% directly to creators. Future Waves versions may take up to 20% for platform expenses.',
+  },
+  {
+    icon: GrMultiple,
+    title: 'Multi-Streaming',
+    description:
+      'Waves aims to empower streamerw by providing tools to stream on all other platforms. Currently, waves supports multistreaming to YouTube, TikTok, and Twitch, with more platforms possible via requests.',
+  },
+  {
+    icon: PiUsersThreeDuotone,
+    title: 'Community Oriented',
+    description:
+      'Waves prioritizes user experience and plans to democratize social media through on-chain voting to determine feature development and the platforms direction.',
+  },
+  {
+    icon: VscLink,
+    title: 'Blockchain Social',
+    description:
+      'Waves leverages blockchain technology to facilitate interoperability between platforms, ensuring that your content remains accessible across all Deso Apps and is Censorship Resistant.',
+  },
+  {
+    icon: TbMoneybag,
+    title: 'NFT Streams & Clips',
+    description:
+      'Future versions of Waves will support NFT Streams and Clips, giving streamers greater longevity and complete control over pricing and royalty percentages of any future sales of the NFT.',
+  },
+];
+
+export function Feature({ icon: Icon, title, description }) {
+  return (
+    <>
+    <div>
+      <ThemeIcon variant="light" size={40} radius={40}>
+        <Icon style={{ width: rem(18), height: rem(18) }} stroke={1.5} />
+      </ThemeIcon>
+      <Text fw={500} mt="sm" mb={7}>
+        {title}
+      </Text>
+      <Text size="sm" c="dimmed" lh={1.6}>
+        {description}
+      </Text>
+    </div>
+    <Space h="xs"/>
+    </>
+  );
+} 
 
 export function Welcome() {
   const { classes } = useStyles();
+  const features = MOCKDATA.map((feature, index) => <Feature {...feature} key={index} />);
 
   return (
     <Card shadow="xl" p="xl" radius="xl" withBorder>
-      <Center>
-        <div className={classes.inner}>
-          <div className={classes.content}>
-            <Title
-              variant="gradient"
-              gradient={{ from: "indigo", to: "cyan" }}
-              order={1}
-              weight={2222}
-              align="center"
-            >
-              Waves
-            </Title>
-            <Divider my="sm" />
-            <Divider
-              my="xs"
-              variant="dashed"
-              labelPosition="center"
-              label={
-                <>
-                  <Title c="dimmed" order={3} weight={777} align="center">
-                    Decentralized Social Literacy
-                  </Title>
-                </>
-              }
-            />
+      
+      <Title className={classes.title} align="center"   variant="gradient"
+      gradient={{ from: 'blue', to: 'cyan', deg: 90 }}>Waves</Title>
+<Space h="sm"/>
+<Container size={560} p={0}>
+  <Text size="xl" fw={700} c="dimmed" align="center">
+    Decentralized Live-Streaming Platform
+  </Text>
+</Container>
+<Divider my="sm" />
 
-            <Text color="dimmed" mt="md">
-              By embracing decentralized social media, you can take control of
-              your online presence and enjoy a more equitable, secure, and
-              transparent experience. With decentralized social media, you'll
-              have access to the following benefits:
-            </Text>
+<SimpleGrid
+  mt={60}
+  cols={{ base: 1, sm: 2, md: 3 }}
+  spacing={{ base: 'xl', md: 50 }}
+  verticalSpacing={{ base: 'xl', md: 50 }}
+>
+  {features}
+</SimpleGrid>
+        
+<Space h="xs"/>
 
-            <List
-              mt={30}
-              spacing="sm"
-              size="sm"
-              icon={
-                <ThemeIcon size={20} radius="xl">
-                  <IconCheck size={12} stroke={1.5} />
-                </ThemeIcon>
-              }
-            >
-              <List.Item>
-                <b>Open Source</b> – Decentralized social media encourages
-                open-source applications and algorithms, allowing for greater
-                transparency and fairness in the digital world.
-              </List.Item>
-              <List.Item>
-                <b>Open Data</b> – Say goodbye to companies privately owning and
-                selling your data. With the DeSo Blockchain, your data is freely
-                available to you.
-              </List.Item>
-              <List.Item>
-                <b>Monetization</b> – Get paid for your contributions on social
-                media with Social-Tips, NFTs, Creator-Coins, and cash-out to USD
-                with ease.
-              </List.Item>
-              <List.Item>
-                <b>Content Ownership</b> – Content is stored on-chain, meaning
-                its not platform exclusive, this ensures ownership of what you
-                create.
-              </List.Item>
-              <List.Item>
-                <b>Interconnected</b> – No more having to rebuild your following
-                as new social medias arise. Your followers and content will be
-                accessible across every app on the DeSo Blockchain.
-              </List.Item>
-              <List.Item>
-                <b>No Investment Needed</b> – Decentralized social media offers
-                an accessible and affordable way to benefit from this
-                revolutionary technology, without ever having to invest any
-                money.
-              </List.Item>
-
-              <List.Item>
-                <b>Direct to Creator</b> – Say goodbye to intermediaries taking
-                huge chunks of revenue from content creators. Decentralized
-                social media ensures that all payments go directly to the
-                creators you support.
-              </List.Item>
-            </List>
-          </div>
-        </div>
-      </Center>
+          
+      
       <Group spacing="xs" grow>
-        <Button
-          variant="default"
-          radius="lg"
-          onClick={() => {
-            identity
-              .login({
-                getFreeDeso: true,
-              })
-              .catch((err) => {
-                if (err?.type === ERROR_TYPES.NO_MONEY) {
-                  alert("You need DESO in order to post!");
-                } else {
-                  alert(err);
-                }
-              });
-          }}
-        >
-          Login
-        </Button>
+        
 
         <Button
           leftIcon={<GiWaveCrest size="1rem" />}
